9dfbbccb30f166b424ad17c7bb93cdf9872b9147
Kalle M. Aagaard <git@k-moeller.dk>
2026-08-18 12:30:32 +0200

    Version updated from 0.1.3 to 0.1.3.1

--- src/type_url/__init__.py
+++ src/type_url/__init__.py
@@ -13,7 +13,7 @@ else:
     type GetCoreSchemaHandler = object
     type CoreSchema = object
 
-__version__ = "0.1.3"
+__version__ = "0.1.3.1"
 
 
 def _make_url(default: URLConfig, url: str) -> URL:
